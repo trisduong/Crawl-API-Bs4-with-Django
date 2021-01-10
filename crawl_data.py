2197f2fd2d5b431cb27f0ce3504ea50390c1b560
Tris Duong <70458716+trisduong@users.noreply.github.com>
2021-01-10 14:40:22 +0700

    update

--- crawl_data.py
+++ crawl_data.py
@@ -2,33 +2,31 @@ import requests
 import sqlite3
 
 
-def get_jobs(page, token):
-    url = 'https://api.github.com/repos/awesome-jobs/vietnam/issues?page={}&q=is%3Aissue+is%3Aopen?access_token={}'.format(page, token)
-    with open('token.txt') as f:
-        token = f.read()
+def get_jobs(page):
+    url = 'https://api.github.com/repos/awesome-jobs/vietnam/issues?page=2&q=is%3Aissue+is%3Aopen'.format(page)
     resp = requests.get(url)
     data = resp.json()
     return data
 
 
 def main():
-    id = 1
     page = 1
-    conn = sqlite3.connect("db.sqlite3")
-    c = conn.cursor()
+    my_id = 1
     while True:
-        jobs = get_jobs(page, token)
-        if jobs != '':
+        jobs = get_jobs(page)
+        if jobs != []:
             for job in jobs:
+                conn = sqlite3.connect("db.sqlite3")
+                c = conn.cursor()
                 title = job['title']
                 detail = job['body']
-                c.execute('INSERT INTO jobs VALUES (?, ?, ?)', (id, title, detail))
-                id += 1
+                c.execute('INSERT INTO awesomejob_awesome_job VALUES (?, ?, ?)', (my_id, title, detail))
+                conn.commit()
+                conn.close()
+                my_id += 1
         else:
             break
         page += 1
-    conn.commit()
-    conn.close()
 
 
 if __name__ == "__main__":
